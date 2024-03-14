6e13c733ac61a63c6b234e3533e56f589e2caedb
FDIL <fdil.fadilh1@gmail.com>
2024-03-14 17:07:34 -0600

    Initial commit
    Signed-off-by: FDIL <fdil.fadilh1@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import NavBar from "./NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <NavBar/>
+
+        <h1>Fadil's Portfolio</h1>
+        <p>I'm hosted with GitHub Pages.</p>
+
+        <h3>
+          <a href="https://github.com/FDIL501st">My GitHub</a>
+        </h3>
+        <br/><br/>
+
+        <h3>Projects worked on</h3>
+        {/*// <!-- Links to projects I want to showcase -->*/}
+        <a href="https://github.com/FDIL501st/Phantom-Forces-damage-ranges-calculator">Phantom Forces Ranges Calculator
+          (Python)</a>
+        <br/><br/>
+
+        <a href="https://github.com/FDIL501st/ENSF480-GR31-TermProject">Movie Ticket booking system (Java)</a>
+        <br/><br/>
+
+        <a href="https://github.com/FDIL501st/ENCM511_gr6_Project-Codes">Small PIC32 projects</a>
+      </div>
   );
 }
 
---
+++ src/NavBar.js
@@ -0,0 +1,8 @@
+const NavBar = () => {
+	return (
+		// TODO: Actually implement a navigation bar
+		<h1>Hello from React!</h1>
+	)
+}
+
+export default NavBar
