cf6b0cdcd84cb7c3ed9c54a75ac8c07be226c4e4
FDIL <fdil.fadilh1@gmail.com>
2024-04-11 21:55:18 -0600

    Update project links to go light-grey when hovering over them.

--- src/components/ProjectLink.jsx
+++ src/components/ProjectLink.jsx
@@ -6,7 +6,7 @@ const ProjectLink = ({link, display_name, description}) => {
 
 
     return (
-        <div className="w3-card w3-padding w3-margin w3-round-xlarge"
+        <div className="w3-card w3-padding w3-margin w3-round-xlarge w3-hover-light-grey"
                 onClick={() => {setShow(prevValue => !prevValue)}}
         >
             <a href={link} className="w3-hover-text-indigo">{display_name}</a>
