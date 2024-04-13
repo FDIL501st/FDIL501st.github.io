7eefb63ae3770940e4564e8a59964f77f397fc99
FDIL <fdil.fadilh1@gmail.com>
2024-04-13 02:48:48 -0600

    Enlargen Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,8 +4,8 @@ import { Link } from 'react-router-dom'
 
 const NavBar = () => {
 	return (
-		<div class="w3-bar w3-black" id="nav">
-			<Link to="/" class="w3-bar-item w3-button w3-mobile">Home</Link>
+		<div class="w3-bar w3-black w3-xlarge" id="nav">
+			<Link to="/" class="w3-bar-item w3-button w3-mobile w3-padding-16">Home</Link>
 		</div>
 	)
 }
