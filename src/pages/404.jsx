a2f7a69b7c5fa427ee0fa7478365436ba86595a1
FDIL <fdil.fadilh1@gmail.com>
2024-09-23 21:50:41 -0600

    Updates

--- src/pages/404.jsx
+++
@@ -1,9 +0,0 @@
-import "../w3.css"
-
-const Page404 = () => {
-    return (
-        <h1>404 - Page does not exist</h1>
-    )
-}
-
-export default Page404
--- src/shared/id.js
+++
@@ -1,5 +0,0 @@
-// has shared html element ids
-
-export const nav_id = "nav"
-
-export const footer_id = "footer"
